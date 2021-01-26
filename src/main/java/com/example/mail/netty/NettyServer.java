c1b2989f5afe9186c3451820d1dca6dba45fa8ef
ttl <tong_ltt@163.com>
2021-01-26 15:54:05 +0800

    modify info

---
+++ src/main/java/com/example/mail/netty/NettyServer.java
@@ -0,0 +1,32 @@
+package com.example.mail.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class NettyServer {
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup,workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG,128)
+                .childOption(ChannelOption.SO_KEEPALIVE,true)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline().addLast(null);
+                    }
+                });
+        System.out.println("server is ready...");
+        final ChannelFuture channelFuture = serverBootstrap.bind(8888).sync();
+
+        channelFuture.channel().closeFuture().sync();
+    }
+}
